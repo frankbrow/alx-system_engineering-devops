c61dc410ae8f268f141ae7ba6db475987b78f9f1
frankbrow <frankamedulife@gmail.com>
2024-05-14 06:50:57 -0700

    puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# make changes to configurationfile using puppet
+
+file { 'etc/shh/ssh_config':
+       ensure => present,
+
+content =>"
+
+      #ssh client configuration 
+      host*
+      IdentityFile ~/.ssh/school
+      passwordAuthentication no
+}
